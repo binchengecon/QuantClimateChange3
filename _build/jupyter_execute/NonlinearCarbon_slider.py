a1d114d4631c56690217483e85e6be74adb207e2
binchengecon <bin.cheng@tse-fr.eu>
2022-10-04 16:19:45 -0500

    initial

--- _build/jupyter_execute/NonlinearCarbon_slider.py
+++ _build/jupyter_execute/NonlinearCarbon_slider.py
@@ -6,24 +6,60 @@
 # In[1]:
 
 
-from Module_widget import *
-from Module_model import *
-from Module_plot import *
+from PIL import Image
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
+import numpy as np
 
 import warnings
 warnings.filterwarnings('ignore')
-
-display(Param_Panel)
-# display(simulate_box_external_run)
 
 
 # In[2]:
 
 
-all_var_names = ['Temperature Anomaly', 'Carbon Concentration Dynamics', 'Carbon Emission', 'Impulse Response Function']
-selected_index = [all_var_names.index(element) for element in simulate_external.value]
+get_ipython().run_line_magic('matplotlib', 'inline')
 
-fig, ax = plot_simulation(pulse_size.value, pulse_year.value,pulse_baseline.value,cearth.value, selected_index)
-plt.tight_layout()
-plt.show()
+@interact
+def image(pulse=(0, 99), cearth=np.array((0.3725, 0.3916, 15.0)), baseline=["rcp60co2eqv3.csv", "rcp00co2eqv3.csv", "carbonvoid.csv"], year=np.array((1801, 2010))):
+    Figure_Dir = "./figure/NC_PulseExp/"
+    # baseline = "rcp60co2eqv3.csv"
+    # cearth=15.0
+    # year=1801
+    filename = Figure_Dir+"Baseline="+baseline+",cearth=" +         str(cearth)+",year="+str(year)+",pulse="+str(pulse)+".png"
+    return Image.open(filename)
+
+
+# In[3]:
+
+
+pulse=1
+Figure_Dir = "./figure/NC_PulseExp/"
+# Figure_Dir = "./NC_PulseExp/"
+# Figure_Dir = ""
+cearth=15.0
+baseline = "rcp60co2eqv3.csv"
+year = 1801
+
+filename = Figure_Dir+"Baseline="+baseline+",cearth=" +     str(cearth)+",year"+str(year)+",pulse="+str(pulse)+".png"
+# Image("test.png")
+filename
+# Image(filename)
+Image("./figure/NC_PulseExp/Baseline=rcp60co2eqv3.csv,cearth=0.3725,year1801,pulse=29.png")
+
+
+# In[6]:
+
+
+from PIL import Image
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+img = Image.open('test.png').convert('L')
+
+
+@interact
+def binarize(th: (0, 255, 1)):
+    return img.point(lambda p: 255 if p > th else 0)
 
