6cf76f59dc2f6ae00e2b60361a85ca35265b4bde
binchengecon <bin.cheng@tse-fr.eu>
2022-10-04 19:02:38 -0500

    initial

--- _build/jupyter_execute/NonlinearCarbon_slider.py
+++ _build/jupyter_execute/NonlinearCarbon_slider.py
@@ -29,37 +29,3 @@ def image(pulse=(0, 99), cearth=np.array((0.3725, 0.3916, 15.0)), baseline=["rcp
     filename = Figure_Dir+"Baseline="+baseline+",cearth=" +         str(cearth)+",year="+str(year)+",pulse="+str(pulse)+".png"
     return Image.open(filename)
 
-
-# In[3]:
-
-
-pulse=1
-Figure_Dir = "./figure/NC_PulseExp/"
-# Figure_Dir = "./NC_PulseExp/"
-# Figure_Dir = ""
-cearth=15.0
-baseline = "rcp60co2eqv3.csv"
-year = 1801
-
-filename = Figure_Dir+"Baseline="+baseline+",cearth=" +     str(cearth)+",year"+str(year)+",pulse="+str(pulse)+".png"
-# Image("test.png")
-filename
-# Image(filename)
-Image("./figure/NC_PulseExp/Baseline=rcp60co2eqv3.csv,cearth=0.3725,year1801,pulse=29.png")
-
-
-# In[6]:
-
-
-from PIL import Image
-import ipywidgets as widgets
-from ipywidgets import interact, interactive, fixed, interact_manual
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-img = Image.open('test.png').convert('L')
-
-
-@interact
-def binarize(th: (0, 255, 1)):
-    return img.point(lambda p: 255 if p > th else 0)
-
