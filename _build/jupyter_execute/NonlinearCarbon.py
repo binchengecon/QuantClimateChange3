79f7b50d1bf43a2017909c5ff310696b30a69e58
binchengecon <bin.cheng@tse-fr.eu>
2022-10-06 16:01:32 -0500

    initial

--- _build/jupyter_execute/NonlinearCarbon.py
+++ _build/jupyter_execute/NonlinearCarbon.py
@@ -6,24 +6,39 @@
 # In[1]:
 
 
-from Module_widget import *
-from Module_model import *
-from Module_plot import *
+from PIL import Image
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
+import numpy as np
 
 import warnings
 warnings.filterwarnings('ignore')
-
-display(Param_Panel)
-display(simulate_box_external_run)
 
 
 # In[2]:
 
 
-all_var_names = ['Temperature Anomaly', 'Carbon Concentration Dynamics', 'Carbon Emission', 'Impulse Response Function']
-selected_index = [all_var_names.index(element) for element in simulate_external.value]
+get_ipython().run_line_magic('matplotlib', 'inline')
 
-fig, ax = plot_simulation(pulse_size.value, pulse_year.value,pulse_baseline.value,cearth.value, selected_index)
-plt.tight_layout()
-plt.show()
+# def image(pulse=(0, 99), cearth=np.array((0.3725, 0.3916, 15.0)), baseline=["carbonvoid.csv",  "rcp00co2eqv3.csv", "rcp30co2eqv3.csv", "rcp45co2eqv3.csv", "rcp60co2eqv3.csv"], year=np.array((1801, 2010)), option=["Trad", "IRF", "IRFstand", "IRFTera"]):
+
+@interact
+def image(pulse=(0, 49), cearth=np.array((0.3916, 15.0)), baseline=["carbonvoid.csv",  "rcp00co2eqv3.csv", "rcp30co2eqv3.csv", "rcp45co2eqv3.csv", "rcp60co2eqv3.csv"], year=np.array((1801,2010)), option=["Trad", "IRF", "IRFstand", "IRFTera"]):
+    Figure_Dir = "./figure/NC_PulseExp/"
+    # baseline = "rcp60co2eqv3.csv"
+    # cearth=15.0
+    # year=1801
+
+    if option=="Trad":
+        filename = Figure_Dir+"Baseline="+baseline+",cearth=" +             str(cearth)+",year="+str(year)+",pulse="+str(pulse)+".png"
+    if option == "IRF":
+        filename = Figure_Dir+"Baseline="+baseline+",cearth=" +             str(cearth)+",year="+str(year)+",pulse=" +             str(pulse)+",2IRF.png"    # return Image.open(filename)
+    if option =="IRFstand":
+        filename = Figure_Dir+"Baseline="+baseline+",cearth=" +             str(cearth)+",year="+str(year)+",pulse=" +             str(pulse)+",2IRF,per.png"    # return Image.open(filename)
+    if option =="IRFTera":
+        filename = Figure_Dir+"Baseline="+baseline+",cearth=" +             str(cearth)+",year="+str(year)+",pulse=" +             str(pulse)+",2IRF,Tera.png"    # return Image.open(filename)
+    userimage = Image.open(filename)
+    # userimage_resize = userimage.resize((800, 1000))
+    # return userimage_resize
+    return userimage
 
