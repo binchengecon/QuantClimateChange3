588d8ab24b6a6e463f3323c22ef3e0c93ad6abda
binchengecon <bin.cheng@tse-fr.eu>
2022-10-06 14:57:12 -0500

    initial

---
+++ _build/jupyter_execute/old/NonlinearCarbon.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Nonlinear Carbon Dynamics
+
+# In[1]:
+
+
+from Module_widget import *
+from Module_model import *
+from Module_plot import *
+
+import warnings
+warnings.filterwarnings('ignore')
+
+display(Param_Panel)
+display(simulate_box_external_run)
+
+
+# In[2]:
+
+
+all_var_names = ['Temperature Anomaly', 'Carbon Concentration Dynamics', 'Carbon Emission', 'Impulse Response Function']
+selected_index = [all_var_names.index(element) for element in simulate_external.value]
+
+fig, ax = plot_simulation(pulse_size.value, pulse_year.value,pulse_baseline.value,cearth.value, selected_index)
+plt.tight_layout()
+plt.show()
+
